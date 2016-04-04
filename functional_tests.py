36b55fbecc1189e6ae8aa9c3ba5f6378f49d7f29
andbar <barringera@gmail.com>
2016-04-04 13:20:25 -0400

    FT now checks we can input to-do item and fails

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +17,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # He is invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # He types "Buy new guitar strings" into a text box
+        inputbox.send_keys('Buy new guitar strings')
 
         # When he hits enter, the page updates, and now the page lists
         # "1: Buy new guitar strings" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy new guitar strings' for row in rows)
+        )
 
         # There is still a text box inviting him to add another item.
         # He enters "Restring guitar"
+        self.fail('Finish the test')
 
         # The page updates again, and now shows both items on his list
 
